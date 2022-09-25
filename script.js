feb7741ca74c2de63c8050bb00e8254590ca67ca
BootstrapFootkicker <89753423+BootstrapFootkicker@users.noreply.github.com>
2022-09-25 16:03:09 -0600

    Add PlayRound function
    Created function to play a round. Function compares player's choice and computer's choice then returns win or loss string based on values.

--- script.js
+++ script.js
@@ -5,4 +5,41 @@ function getComputerChoice() {
     return choices[randomVal];
 }
 
+function playRound(playerSelection, computerSelection) {
+    playerSelection = playerSelection.toLowerCase();
+    computerSelection = computerSelection.toLowerCase();
+    if (playerSelection === computerSelection) {
+        console.log(`Draw! You both chose ${playerSelection}`);
+        return `Draw! You both chose ${playerSelection}`;
+    } else if (playerSelection === "rock") {
+        if (computerSelection === "scissors") {
+            console.log(`You Win! ${playerSelection} beats ${computerSelection}`);
+            return `You Win! ${playerSelection} beats ${computerSelection}`;
+        } else {
+            console.log(`You Lose! ${computerSelection} beats ${playerSelection}`);
+            return `You Lose! ${computerSelection} beats ${playerSelection}`;
+        }
+    } else if (playerSelection === "scissors") {
+        if (computerSelection === "paper") {
+            console.log(`You Win! ${playerSelection} beats ${computerSelection}`);
+            return `You Win! ${playerSelection} beats ${computerSelection}`;
+        } else {
+            console.log(`You Lose! ${computerSelection} beats ${playerSelection}`);
+            return `You Lose! ${computerSelection} beats ${playerSelection}`;
+        }
+    } else if (playerSelection === "paper") {
+        if (computerSelection === "rock") {
+            console.log(`You Win! ${playerSelection} beats ${computerSelection}`);
+            return `You Win! ${playerSelection} beats ${computerSelection}`;
+        } else {
+            console.log(`You Lose! ${computerSelection} beats ${playerSelection}`);
+            return `You Lose! ${computerSelection} beats ${playerSelection}`;
+        }
 
+    }
+
+}
+
+for (let i = 0; i < 6; i++) {
+    playRound('rock', getComputerChoice());
+}
